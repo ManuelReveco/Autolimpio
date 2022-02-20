c0f43cb7dbe7989c26bea848ec7c1f9787b643cd
ManuelReveco <manuel.reveco@gmail.com>
2022-02-20 02:50:51 -0300

    archivo con los calculos en jquery

---
+++ js/formulario.js
@@ -0,0 +1,38 @@
+const boton = document.querySelector("#btnCalcular");
+const montoCancelar = document.querySelector("#montoCancelar");
+const chkExterior = document.querySelector("#chkExterior");
+const chkMotor = document.querySelector("#chkMotor");
+const txtNombre = document.querySelector("#txtNombre");
+const txtApellido = document.querySelector("#txtApellido");
+
+boton.addEventListener("click", function (evento) {
+    let pago1 = 0;
+    let pago2 = 0;
+    let subTotal = 0;
+    let iva=0;
+    let total=0
+    if(chkExterior.checked ||chkMotor.checked)
+    {
+        if (chkExterior.checked) {
+           
+            pago1 = 7000;
+    
+        }
+        if (chkMotor.checked) {
+            
+            pago2 = 5000;
+        }
+        subTotal = pago1 + pago2;
+        iva=subTotal*19/100;
+        total=subTotal+iva;
+        alert(txtNombre.value+" "+txtApellido.value+"/n total sin iva $ "+subTotal+" iva $"+iva+" total a pagar $ "+total);
+        //montoCancelar.value = total;
+    
+    }
+    else
+    {
+        alert("debe seleecionar el servicio");
+        //montoCancelar.value = "debe seleccionar un servicio";
+    }
+    
+});
